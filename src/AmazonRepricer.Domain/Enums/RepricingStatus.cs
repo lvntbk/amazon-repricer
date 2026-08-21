93e017c808a40576d01ac8cfcb9967fa8b5ae9f4
Levent Ince <leventince0303@gmail.com>
2026-08-21 16:15:30 +0300

    feat: add repricing approval workflow

---
+++ src/AmazonRepricer.Domain/Enums/RepricingStatus.cs
@@ -0,0 +1,10 @@
+namespace AmazonRepricer.Domain.Enums;
+
+public enum RepricingStatus
+{
+    Pending = 1,
+    Approved = 2,
+    Rejected = 3,
+    Applied = 4,
+    Failed = 5
+}
--- src/AmazonRepricer.Infrastructure/Persistence/Migrations/20260821131040_AddRepricingApprovalWorkflow.Designer.cs
+++ src/AmazonRepricer.Infrastructure/Persistence/Migrations/20260821131040_AddRepricingApprovalWorkflow.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AmazonRepricer.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AmazonRepricer.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(RepricerDbContext))]
-    partial class RepricerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260821131040_AddRepricingApprovalWorkflow")]
+    partial class AddRepricingApprovalWorkflow
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
